f4ee2492961cbbc43fb48f98dda67f29fad955e0
asadu <testlead.asad@gmail.com>
2017-11-15 21:47:52 -0500

    reduced step

--- features/bluefly_homepage.feature
+++ features/bluefly_homepage.feature
@@ -7,45 +7,45 @@ Feature: As a user verify All dropdown menu from Bluefly homepage
 
   @rony
   Scenario:Verify DESIGNERS dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the DESIGNERS option,
-    And I see Dropdown the menu from DESIGNERS option
+    Then I Place the mouse pointer over the "DESIGNERS" option,
+    And I see Dropdown the menu from "DESIGNERS" option
   @rony
   Scenario:Verify WOMEN dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the WOMEN option,
-    And I see Dropdown the menu from WOMEN option
+    Then I Place the mouse pointer over the "WOMEN" option,
+    And I see Dropdown the menu from "WOMEN" option
   @rony
   Scenario:Verify SHOES dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the SHOES option,
-    And I see Dropdown the menu from SHOES option
+    Then I Place the mouse pointer over the "SHOES" option,
+    And I see Dropdown the menu from "SHOES" option
   @rony
   Scenario:Verify HANDBAGS & ACCESSORIES dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the HANDBAGS & ACCESSORIES option,
-    And I see Dropdown the menu from HANDBAGS & ACCESSORIES option
+    Then I Place the mouse pointer over the "HANDBAGS & ACCESSORIES" option,
+    And I see Dropdown the menu from "HANDBAGS & ACCESSORIES" option
   @rony
   Scenario:Verify VINTAGE dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the VINTAGE option,
-    And I see Dropdown the menu from VINTAGE option
+    Then I Place the mouse pointer over the "VINTAGE" option,
+    And I see Dropdown the menu from "VINTAGE" option
   @rony
   Scenario:Verify JEWELRY dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the JEWELRY option,
-    And I see Dropdown the menu from JEWELRY option
+    Then I Place the mouse pointer over the "JEWELRY" option,
+    And I see Dropdown the menu from "JEWELRY" option
   @rony
   Scenario:Verify BEAUTY dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the BEAUTY option,
-    And I see Dropdown the menu from BEAUTY option
+    Then I Place the mouse pointer over the "BEAUTY" option,
+    And I see Dropdown the menu from "BEAUTY" option
   @rony
   Scenario:Verify MEN dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the MEN option,
-    And I see Dropdown the menu from MEN option
+    Then I Place the mouse pointer over the "MEN" option,
+    And I see Dropdown the menu from "MEN" option
   @rony
   Scenario:Verify HOME dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the HOME option,
-    And I see Dropdown the menu from HOME option
+    Then I Place the mouse pointer over the "HOME" option,
+    And I see Dropdown the menu from "HOME" option
   @rony
   Scenario:Verify HOLIDAY GIFT GUIDE dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the HOLIDAY GIFT GUIDE option,
-    And I see Dropdown the menu from HOLIDAY GIFT GUIDE option
+    Then I Place the mouse pointer over the "HOLIDAY GIFT GUIDE" option,
+    And I see Dropdown the menu from "HOLIDAY GIFT GUIDE" option
   @rony
   Scenario:Verify CLEARANCE dropdown option displayed in the bluefly homepage
-    Then I Place the mouse pointer over the CLEARANCE option,
-    And I see Dropdown the menu from CLEARANCE option
+    Then I Place the mouse pointer over the "CLEARANCE" option,
+    And I see Dropdown the menu from "CLEARANCE" option
--- features/page_definitions/bluefly_homepage.rb
+++ features/page_definitions/bluefly_homepage.rb
@@ -23,135 +23,134 @@ Then("I dismiss popup if dispalyed") do
 
 end
 
-Then("I Place the mouse pointer over the DESIGNERS option,") do
-  $driver.action.move_to($driver.find_element(link: 'DESIGNERS')).perform
+Then("I Place the mouse pointer over the {string} option,") do |string|
+  $driver.action.move_to($driver.find_element(link: string)).perform
 end
 
-Then("I see Dropdown the menu from DESIGNERS option") do
- if $driver.find_element(link: 'DESIGNERS').displayed?
-   puts "DESIGNER STEP PASS"
+Then("I see Dropdown the menu from {string} option") do |string|
+ if $driver.find_element(link: string).displayed?
+   puts "#{string} STEP PASS"
  else
-   puts "DESIGNER STEP FAIL"
+   puts "#{string} STEP FAIL"
  end
-
 end
 
-Then("I Place the mouse pointer over the WOMEN option,") do
-  $driver.action.move_to($driver.find_element(link: 'WOMEN')).perform
-end
+# Then("I Place the mouse pointer over the WOMEN option,") do
+#   $driver.action.move_to($driver.find_element(link: 'WOMEN')).perform
+# end
 
-Then("I see Dropdown the menu from WOMEN option") do
-  if $driver.find_element(link: 'WOMEN').displayed?
-    puts "WOMEN STEP PASS"
-  else
-    puts "WOMEN STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from WOMEN option") do
+#   if $driver.find_element(link: 'WOMEN').displayed?
+#     puts "WOMEN STEP PASS"
+#   else
+#     puts "WOMEN STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the SHOES option,") do
-  $driver.action.move_to($driver.find_element(link: 'SHOES')).perform
-end
+# Then("I Place the mouse pointer over the SHOES option,") do
+#   $driver.action.move_to($driver.find_element(link: 'SHOES')).perform
+# end
 
-Then("I see Dropdown the menu from SHOES option") do
-  if $driver.find_element(link: 'SHOES').displayed?
-    puts "SHOES STEP PASS"
-  else
-    puts "SHOES STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from SHOES option") do
+#   if $driver.find_element(link: 'SHOES').displayed?
+#     puts "SHOES STEP PASS"
+#   else
+#     puts "SHOES STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the HANDBAGS & ACCESSORIES option,") do
-  $driver.action.move_to($driver.find_element(link: 'HANDBAGS & ACCESSORIES')).perform
-end
+# Then("I Place the mouse pointer over the HANDBAGS & ACCESSORIES option,") do
+#   $driver.action.move_to($driver.find_element(link: 'HANDBAGS & ACCESSORIES')).perform
+# end
 
-Then("I see Dropdown the menu from HANDBAGS & ACCESSORIES option") do
-  if $driver.find_element(link: 'HANDBAGS & ACCESSORIES').displayed?
-    puts "HANDBAGS & ACCESSORIES STEP PASS"
-  else
-    puts "HANDBAGS & ACCESSORIES STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from HANDBAGS & ACCESSORIES option") do
+#   if $driver.find_element(link: 'HANDBAGS & ACCESSORIES').displayed?
+#     puts "HANDBAGS & ACCESSORIES STEP PASS"
+#   else
+#     puts "HANDBAGS & ACCESSORIES STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the VINTAGE option,") do
-  $driver.action.move_to($driver.find_element(link: 'VINTAGE')).perform
-end
+# Then("I Place the mouse pointer over the VINTAGE option,") do
+#   $driver.action.move_to($driver.find_element(link: 'VINTAGE')).perform
+# end
 
-Then("I see Dropdown the menu from VINTAGE option") do
-  if $driver.find_element(link: 'VINTAGE').displayed?
-    puts "VINTAGE STEP PASS"
-  else
-    puts "VINTAGE STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from VINTAGE option") do
+#   if $driver.find_element(link: 'VINTAGE').displayed?
+#     puts "VINTAGE STEP PASS"
+#   else
+#     puts "VINTAGE STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the JEWELRY option,") do
-  $driver.action.move_to($driver.find_element(link: 'JEWELRY')).perform
-end
+# Then("I Place the mouse pointer over the JEWELRY option,") do
+#   $driver.action.move_to($driver.find_element(link: 'JEWELRY')).perform
+# end
 
-Then("I see Dropdown the menu from JEWELRY option") do
-  if $driver.find_element(link: 'JEWELRY').displayed?
-    puts "JEWELRY STEP PASS"
-  else
-    puts "JEWELRY STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from JEWELRY option") do
+#   if $driver.find_element(link: 'JEWELRY').displayed?
+#     puts "JEWELRY STEP PASS"
+#   else
+#     puts "JEWELRY STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the BEAUTY option,") do
-  $driver.action.move_to($driver.find_element(link: 'BEAUTY')).perform
-end
+# Then("I Place the mouse pointer over the BEAUTY option,") do
+#   $driver.action.move_to($driver.find_element(link: 'BEAUTY')).perform
+# end
 
-Then("I see Dropdown the menu from BEAUTY option") do
-  if $driver.find_element(link: 'BEAUTY').displayed?
-    puts "BEAUTY STEP PASS"
-  else
-    puts "BEAUTY STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from BEAUTY option") do
+#   if $driver.find_element(link: 'BEAUTY').displayed?
+#     puts "BEAUTY STEP PASS"
+#   else
+#     puts "BEAUTY STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the MEN option,") do
-  $driver.action.move_to($driver.find_element(link: 'MEN')).perform
-end
+# Then("I Place the mouse pointer over the MEN option,") do
+#   $driver.action.move_to($driver.find_element(link: 'MEN')).perform
+# end
 
-Then("I see Dropdown the menu from MEN option") do
-  if $driver.find_element(link: 'MEN').displayed?
-    puts "MEN STEP PASS"
-  else
-    puts "MEN STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from MEN option") do
+#   if $driver.find_element(link: 'MEN').displayed?
+#     puts "MEN STEP PASS"
+#   else
+#     puts "MEN STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the HOME option,") do
-  $driver.action.move_to($driver.find_element(link: 'HOME')).perform
-end
+# Then("I Place the mouse pointer over the HOME option,") do
+#   $driver.action.move_to($driver.find_element(link: 'HOME')).perform
+# end
 
-Then("I see Dropdown the menu from HOME option") do
-  if $driver.find_element(link: 'HOME').displayed?
-    puts "HOME STEP PASS"
-  else
-    puts "HOME STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from HOME option") do
+#   if $driver.find_element(link: 'HOME').displayed?
+#     puts "HOME STEP PASS"
+#   else
+#     puts "HOME STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the HOLIDAY GIFT GUIDE option,") do
-  $driver.action.move_to($driver.find_element(link: 'HOLIDAY GIFT GUIDE')).perform
-end
+# Then("I Place the mouse pointer over the HOLIDAY GIFT GUIDE option,") do
+#   $driver.action.move_to($driver.find_element(link: 'HOLIDAY GIFT GUIDE')).perform
+# end
 
-Then("I see Dropdown the menu from HOLIDAY GIFT GUIDE option") do
-  if $driver.find_element(link: 'HOLIDAY GIFT GUIDE').displayed?
-    puts "HOLIDAY GIFT GUIDE STEP PASS"
-  else
-    puts "HOLIDAY GIFT GUIDE STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from HOLIDAY GIFT GUIDE option") do
+#   if $driver.find_element(link: 'HOLIDAY GIFT GUIDE').displayed?
+#     puts "HOLIDAY GIFT GUIDE STEP PASS"
+#   else
+#     puts "HOLIDAY GIFT GUIDE STEP FAIL"
+#   end
+# end
 
-Then("I Place the mouse pointer over the CLEARANCE option,") do
-  $driver.action.move_to($driver.find_element(link: 'CLEARANCE')).perform
-end
+# Then("I Place the mouse pointer over the CLEARANCE option,") do
+#   $driver.action.move_to($driver.find_element(link: 'CLEARANCE')).perform
+# end
 
-Then("I see Dropdown the menu from CLEARANCE option") do
-  if $driver.find_element(link: 'CLEARANCE').displayed?
-    puts "CLEARANCE STEP PASS"
-  else
-    puts "CLEARANCE STEP FAIL"
-  end
-end
+# Then("I see Dropdown the menu from CLEARANCE option") do
+#   if $driver.find_element(link: 'CLEARANCE').displayed?
+#     puts "CLEARANCE STEP PASS"
+#   else
+#     puts "CLEARANCE STEP FAIL"
+#   end
+# end
